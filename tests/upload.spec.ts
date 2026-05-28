9a7bd03e90ae38b65d169c3cfdc0171b569e2424
Borovnica32 <zverm@protonmail.com>
2026-05-28 11:35:27 +0200

    modify file title

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -23,3 +23,23 @@ test('Modertor upload file', async ({ page }) => {
     await page.getByText('Add torrent').click();
 
 });
+
+test('Modertor change file title', async ({ page }) => {
+    await page.goto('http://saturnmerkur.ddns.net/zverina/internacionala');
+    await expect(page).toHaveTitle(/Glavana staran/);
+
+    await page.locator('a[href="/zverina/mod_main"]').click();
+    await expect(page).toHaveTitle(/Moderator/);
+
+    await page.locator('a[href="mod_main/seznamTorrentov/2"]').click();
+    await page.locator('a[href="/zverina/mod_main/urediTorrent/128"]').click();
+    
+    await expect(page).toHaveTitle(/Moderator/);
+
+    await page.fill('#naslov', 'GitHub Cheat Sheatt');
+
+    await page.getByText('Uredi').click();
+
+    await expect(page).toHaveURL('http://saturnmerkur.ddns.net/zverina/mod_main/seznamTorrentov/2');
+
+});
