94ebc615eece9f78dfd0aaf46b75047e46f6e356
Borovnica32 <zverm@protonmail.com>
2026-05-29 07:59:11 +0200

    error handeling

--- tests/apiAWS.spec.ts
+++ tests/apiAWS.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('GET Samsonite sets API', async ({ request }) => {
-  const dataFrom = 'Samsonite';
+  const dataFrom = 'Samso1nite';
 
   const start = Date.now();
 
@@ -11,15 +11,21 @@ test('GET Samsonite sets API', async ({ request }) => {
 
   const duration = Date.now() - start;
 
-  expect(response.ok()).toBeTruthy();
-
   const body = await response.json();
 
+  if (body.error) {
+    console.log('API ERROR:', body.error);
+    console.log('Available tables:', body.available_tables);
+
+    throw new Error('API returned error: ' + body.error);
+  }
+
+  // Assertions
   await test.step('Validate table name', async() => {
     var tableName: string = body.table;
     
     try{
-      expect(tableName).toBe(dataFrom);
+      expect.soft(tableName).toBe(dataFrom);
       console.log(`[OK] Table name is valid (${tableName})`);
     } catch(e) {
       console.log(`[FAILED] Table name is valid (${tableName})`);
@@ -29,7 +35,7 @@ test('GET Samsonite sets API', async ({ request }) => {
 
   await test.step('Validate if DATA is a array', async() => {
     try{
-      expect(Array.isArray(body.data)).toBeTruthy();
+      expect.soft(Array.isArray(body.data)).toBeTruthy();
       console.log('[OK] Field DATA is a array');
     } catch(e) {
       console.log('[FAILED] Field DATA is a array');
@@ -41,7 +47,7 @@ test('GET Samsonite sets API', async ({ request }) => {
     var dataLength: number = body.data.length;
     
     try{
-      expect(dataLength).toBeGreaterThan(0);
+      expect.soft(dataLength).toBeGreaterThan(0);
       console.log(`[OK] Array DATA has more than 0 entries (${dataLength})`);
     } catch(e) {
       console.log(`[FAILED] Array DATA has more than 0 entries (${dataLength})`);
@@ -53,7 +59,7 @@ test('GET Samsonite sets API', async ({ request }) => {
     var statCode: number = 0;
     try{
       statCode = response.status();
-      expect(statCode).toBe(200);
+      expect.soft(statCode).toBe(200);
       console.log(`[OK] Returned status code is OK (${statCode})`);
     } catch(e) {
       console.log(`[FAILED] Returned status code is OK (${statCode})`);
@@ -63,7 +69,7 @@ test('GET Samsonite sets API', async ({ request }) => {
 
   await test.step('Validate response time', async() => {
     try{
-      expect(duration).toBeLessThan(3000);
+      expect.soft(duration).toBeLessThan(3000);
       console.log(`[OK] Respinse time is less than 3 seconds (${duration*0.001}s)`);
     } catch(e) {
       console.log(`[FAILED] Respinse time is less than 3 seconds (${duration*0.001}s)`);
